fb3a7f82828c128b6ff9e94e3fb9f1a7ddf68e08
name <email>
2025-10-17 11:26:01 +0200

    Assesment complete

--- assessment1.py
+++ assessment1.py
@@ -1,68 +1,25 @@
 def product(num1, num2):
-    """
-    Returns the product of two numbers.
-
-    Parameters:
-        num1 (int): The first number.
-        num2 (int): The second number.
-
-    Returns:
-        int: The result of multiplying num1 by num2.
-    """
-    pass
+    return num1 * num2
 
 
 def square(number):
-    """
-    Returns the square of a number.
-
-    Parameters:
-        number (int): The number to be squared.
-
-    Returns:
-        int: The square of the input number.
-    """
-    pass
+    return number ** 2
 
 
 def even(n):
-    """
-    Checks whether a number is even.
-
-    Parameters:
-        n (int): The number to check.
-
-    Returns:
-        bool: True if n is even, False otherwise.
-    """
-    pass
+    return True if n % 2 == 0 else False
 
 
 def sum_of_list(numbers):
-    """
-    Calculates the sum of all elements in a list.
-
-    Parameters:
-        numbers (list of int): A list of numbers.
-
-    Returns:
-        int: The total sum of all numbers in the list.
-    """
-    pass
+    return sum(numbers)
 
 
 def list_of_even_numbers(numbers):
-    """
-    Returns a list of even numbers from 1 up to (but not including) the given number.
-
-    Parameters:
-        numbers (int): The upper limit of the range to check for even numbers.
-
-    Returns:
-        list of int: A list containing all even numbers less than the given number.
-    """
-    pass
-
+    even_no = []
+    for n in range(1,numbers):
+        if even(n):
+            even_no.append(n)
+    return even_no
 
 def sum_of_odd_numbers(numbers):
     """
@@ -74,4 +31,8 @@ def sum_of_odd_numbers(numbers):
     Returns:
         int: The sum of all odd numbers in the list.
     """
-    pass
+    odd = []
+    for n in numbers:
+        if not even(n):
+            odd.append(n)
+    return sum(odd)
